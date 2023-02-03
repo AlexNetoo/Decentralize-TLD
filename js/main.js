99d4a37baba0d27a8bf11886a50b342a09b4f041
Diogo Pinto <thediogopinto@gmail.com>
2023-02-03 23:48:58 +0000

    Handle redirect if search is valid and Enter key was pressed

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ let searchValue = null;
  * Validates if search value is valid and if search button should be enabled/disabled.
  */
 function listenToSearchInputValue() {
-    domainSearchInput.addEventListener('keyup', () => {
+    domainSearchInput.addEventListener('keyup', (event) => {
         searchValue = `${domainSearchInput.value}`.trim().toLowerCase();
         const searchHasWhitespaces = searchValue.indexOf(' ') > -1;
         let isSearchValid = true;
@@ -20,17 +20,40 @@ function listenToSearchInputValue() {
 
         // Disable button if search value is not valid, otherwise enable it
         domainSearchBtn.disabled = !isSearchValid;
+
+        handleKeyPressOnSearchInput(event.key, isSearchValid);
     });
+}
+
+/**
+ * Checks if `Enter` key was pressed on input.
+ *
+ * If the search value is valid, it will redirect to porkbun.
+ *
+ * @param {string} eventKey
+ * @param {boolean} isSearchValid
+ */
+function handleKeyPressOnSearchInput(eventKey, isSearchValid) {
+    if (eventKey === 'Enter' && isSearchValid) {
+        redirectToPorkbun();
+    }
+}
+
+/**
+ * Redirects to porkbun's website with search value in the query params.
+ */
+function redirectToPorkbun() {
+    window.open(`${porkbunBaseUrl}${searchValue}`);
 }
 
 /**
  * Listens to `click` event of domain search button.
  *
- * When clicked, button will redirect to porkbun's website with search value in the query params.
+ * When clicked, it will redirect to porkbun.
  */
 function listenToSearchButtonClick() {
     domainSearchBtn.addEventListener('click', () => {
-        window.open(`${porkbunBaseUrl}${searchValue}`);
+        redirectToPorkbun();
     });
 }
 
